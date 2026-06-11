cbaee1a4ea23925eb12cc903d1da4ca8a6fd9b04
echavairyu <evafauziaagustinningtiyas@gmail.com>
2026-06-11 17:31:45 +0700

    bissmillah

---
+++ Fishy_Projek/ClassGudang.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fishy_Projek
+{
+    public class Gudang
+    {
+        private int idGudang;
+        private string namaGudang;
+        private string lokasiGudang;
+
+        public Gudang(int idGudang, string namaGudang, string lokasiGudang)
+        {
+            this.idGudang = idGudang;
+            this.namaGudang = namaGudang;
+            this.lokasiGudang = lokasiGudang;
+        }
+
+        public int IdGudang
+        {
+            get { return idGudang; }
+            set { idGudang = value; }
+        }
+
+        public string NamaGudang
+        {
+            get { return namaGudang; }
+            set { namaGudang = value; }
+        }
+
+        public string LokasiGudang
+        {
+            get { return lokasiGudang; }
+            set { lokasiGudang = value; }
+        }
+    }
+}
