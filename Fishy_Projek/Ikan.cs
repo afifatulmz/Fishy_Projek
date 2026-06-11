c732d6daf6c755561915af3c84c51c63f0fc1e2e
afifatulmz <afifatulmuthiahza@gmail.com>
2026-06-11 14:53:21 +0700

    Add project files.

---
+++ Fishy_Projek/Ikan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fishy_Projek
+{
+    public class Ikan
+    {
+        private string idIkan;
+        private string namaIkan;
+        private double suhuIdeal;
+        private double batasSuhu;
+    }
+
+    public Ikan { }
+}
